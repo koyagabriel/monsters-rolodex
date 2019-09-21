cbac7194468d6727872ec24c9d2d0bb4573edc2c
koya gabriel <stakeholder@koyas-MacBook-Pro.local>
2019-09-21 08:44:46 +0100

    Updates

---
+++ precache-manifest.90867b6d94c1541a6c50a1addba768f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a5dcdbe87c97ce7d1d729e660b7647",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f08e5a3aa2b2f647c18e",
+    "url": "/monsters-rolodex/static/css/main.02b75daf.chunk.css"
+  },
+  {
+    "revision": "0792b5e2043d95efda44",
+    "url": "/monsters-rolodex/static/js/2.1159b1ef.chunk.js"
+  },
+  {
+    "revision": "f08e5a3aa2b2f647c18e",
+    "url": "/monsters-rolodex/static/js/main.9570073e.chunk.js"
+  },
+  {
+    "revision": "cd638ab4be3c3f973283",
+    "url": "/monsters-rolodex/static/js/runtime~main.2d5d4edd.js"
+  }
+]);
